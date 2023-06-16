395fa428af435821ea9e80302b9ba53bd2861bb7
Alex Miller <fotoflo@gmail.com>
2023-06-16 17:36:35 +0800

    just a commit to tell the people that i rotated all my keys

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -1,5 +1,4 @@
-const PINECONE_INDEX_NAME = 'notionindex';
-
-const PINECONE_NAME_SPACE = 'notion-langchain'; //namespace is optional for your vectors
+const PINECONE_INDEX_NAME = "notionindex";
+const PINECONE_NAME_SPACE = "notion-langchain"; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
