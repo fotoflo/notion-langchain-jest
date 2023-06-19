cf3b6cc1bcf93170a14cfc84b8a9ea1426a5fc47
Alex Miller <fotoflo@gmail.com>
2023-06-19 19:30:27 +0800

    comment out the old prompt

--- src/chat.ts
+++ src/chat.ts
@@ -6,12 +6,6 @@ import { BufferMemory } from "langchain/memory";
 export const basicChat = async (question: string) => {
   const sanitizedQuestion = question.trim().replaceAll("\n", " ");
 
-  // const prompt = `You are a helpful bot that answers questions about Accelerating Asia.
-  // The following is a conversation with a user.
-  // If there's a link in the doc, please put it in an <a> tag and append it to https://www.notion.so/acceleratingasia/
-  // without the md extension.  Also who should i ask about, please include their contact info in the response
-  // The user is a human and you are a bot. \n\nUser: ${sanitizedQuestion}\nBot:`;
-
   const vectorStore = await getPineconeStore();
   const model = openai;
   // const memory = new BufferWindowMemory();
